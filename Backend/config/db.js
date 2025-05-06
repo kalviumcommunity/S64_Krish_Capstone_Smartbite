8887714bd6ba3ad676ea385ae2410f76e6480995
Krish Agarwal <krish.agarwal@kalvium.community>
2025-05-06 15:16:06 +0530

    Added Mongoose Product schema

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://krishagarwal52139:krish123@cluster0.ltzudax.mongodb.net/fooddevilery website ').then(()=>console.log("DB Connected"));
+export const connectDB = async () =>{
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>{
+       console.log('DB connected') ;
+    })
 }
--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -1,53 +1,54 @@
-import  foodModel from "../models/foodModel.js";
 import fs from 'fs'
+import foodModel from '../models/foodModel.js'
 
 //add food item
 
-const addFood = async (req,res)=>{
+const addFood = async (req,res) =>{
+
     let image_filename = `${req.file.filename}`;
 
     const food = new foodModel({
-        name:req.body.name,
+        name: req.body.name,
         description:req.body.description,
         price:req.body.price,
         category:req.body.category,
         image:image_filename
     })
-    try{
+
+    try {
         await food.save();
-        res.json({success:true,message:"food Added"})
-    }
-    catch(error){
+        res.json({success:true,message:'Food Added'})
+    } catch (error) {
         console.log(error)
-        res.json({success:false,message:"Error"})
+        res.json({success:false, message:'Error'})
     }
 }
 
+// All food list
 
-//all food list
-const listFood = async(req,res)=>{
-    try{
+const listFood = async (req,res) =>{
+    try {
         const foods = await foodModel.find({});
         res.json({success:true,data:foods})
-    }
-    catch(error){
-        console.log(error);
-        res.json({success:false,message:"Error"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:'Error'})
     }
 }
 
-//remove food items
-const removeFood = async(req,res)=>{
-    try{
+// remove food item
+
+const removeFood = async (req,res)=>{
+    try {
         const food = await foodModel.findById(req.body.id);
         fs.unlink(`uploads/${food.image}`,()=>{})
-        await foodModel.findByIdAndDelete(req.body.id);
-        res.json({success:true,message:"Food Removed"})
-    }
-    catch(error){
-        console.log(error);
-        res.json({success:false,message:"Error"})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:'Food Removed'})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:'Error'})
     }
 }
 
-export {addFood,listFood,removeFood}
+export {addFood, listFood, removeFood}
